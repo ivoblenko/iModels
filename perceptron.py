6c15b40800264ed384f40643d1367ad3a42a49f5
Илья Вобленко <ilya.voblenko@gmail.com>
2021-11-02 16:51:03 +0300

    stable version without optimization and separation to classification and regression classes

--- perceptron.py
+++ perceptron.py
@@ -3,20 +3,20 @@ import pandas as pd
 
 
 class Perceptron:
-    def __init__(self, data: pd.DataFrame, classification_mode: bool = False, activation_function_type: str ="sigmoid",
-                 count_of_layers: int =3,
-                 epochs: int =10000):
+    def __init__(self, data: pd.DataFrame, classification_mode: bool = False, activation_function_type: str = "sigmoid",
+                 count_of_hidden_layers: int = 1,
+                 epochs: int = 10000):
 
         """
         :param data: input data for fit model
         :param classification_mode: Flag to activate classification mode
         :param activation_function_type: Type of activation function to neuron
-        :param count_of_layers: Count of hidden layers
+        :param count_of_hidden_layers: Count of hidden layers
         :param epochs: Count of fit's epochs
         """
 
         self.data = data
-        self.step = 0.001
+        self.epsilon = 0.001
         self.input_data = np.resize(self.data.iloc[:, 1:].to_numpy(), (self.data.shape[0], 28 * 28))
         self.output_data = np.resize(self.data.iloc[:, 0].to_numpy(), (self.data.shape[0], 1))
         self.output_layer_size = None
@@ -24,10 +24,11 @@ class Perceptron:
         self.layers = None
         self.weights = None
         self.activation_func = None
-        self.count_of_layers = count_of_layers
+        self.count_of_hidden_layers = count_of_hidden_layers
         self.epochs = epochs
         self.activation_functions = {"sigmoid": self._sigmoid}
-        if classification_mode:
+        self.classification_mode = classification_mode
+        if self.classification_mode:
             self._reorganized_y_to_classification()
 
         self._set_activation_function(activation_function_type)
@@ -40,16 +41,17 @@ class Perceptron:
         self.activation_func = self.activation_functions.get(activation_function, self._sigmoid)
 
     def _set_layers(self):
-        self.layers = [0 for i in range(self.count_of_layers)]
+        self.layers = [0 for i in range(self.count_of_hidden_layers + 2)]
         self.layers[0] = self.input_data
 
     def _set_random_weights(self):
-        self.weights = [0 for i in range(self.count_of_layers - 1)]
-        for i in range(self.count_of_layers - 1):
+        self.weights = [0 for i in range(self.count_of_hidden_layers + 1)]
+        for i in range(self.count_of_hidden_layers + 1):
             if not i:
                 cur_demension = self.layers[i].shape[1]
-            else: cur_demension = next_demesion
-            if i < self.count_of_layers - 2:
+            else:
+                cur_demension = next_demesion
+            if i < self.count_of_hidden_layers:
                 next_demesion = cur_demension // 2
             else:
                 next_demesion = self.output_data.shape[1]
@@ -67,28 +69,36 @@ class Perceptron:
         self.recognized_dict = dict(map(lambda x: [change_dict.get(x), x], change_dict.keys()))
 
     def fit(self):
-        layer_delta = np.array([], float)
+        layer_delta = [0 for i in range(self.count_of_hidden_layers + 1)]
 
         for i in range(self.epochs):
-            for j in range(1, self.count_of_layers):
+            print(i)
+            for j in range(1, self.count_of_hidden_layers + 2):
                 self.layers[j] = self.activation_func(np.dot(self.layers[j - 1], self.weights[j - 1]))
 
-            layer_delta[self.count_of_layers - 1] = (self.layers[self.count_of_layers - 1] - self.output_data) \
-                                                * self.activation_func(self.layers[self.count_of_layers - 1], True)
-            for j in range(self.count_of_layers - 1, 1, -1):
-                layer_delta[j] = layer_delta[j + 1].dot(self.weights[j + 1].T) * self.activation_func(self.layers[j],
-                                                                                                      True)
+            layer_delta[self.count_of_hidden_layers] = (self.layers[self.count_of_hidden_layers + 1] - self.output_data) \
+                                                       * self.activation_func(
+                self.layers[self.count_of_hidden_layers + 1],
+                True)
+            for j in range(self.count_of_hidden_layers - 1, -1, -1):
+                layer_delta[j] = layer_delta[j + 1].dot(self.weights[j + 1].T) * self.activation_func(
+                    self.layers[j + 1],
+                    True)
 
-            for j in range(self.count_of_layers - 1, 1, -1):
-                self.weights[j] -= self.step * self.layers[j - 1].T.dot(layer_delta[j])
+            for j in range(self.count_of_hidden_layers, -1, -1):
+                self.weights[j] -= self.epsilon * self.layers[j].T.dot(layer_delta[j])
 
-    def predict(self, x):
-        self.layers = np.empty([self.count_of_layers])
-        self.layers[0] = np.resize(x, (1, x.size))
-        for i in range(1, self.count_of_layers):
+    def predict(self, input: pd.DataFrame):
+        input = input.to_numpy()
+        self.layers[0] = np.resize(input, (1, input.size))
+        for i in range(1, self.count_of_hidden_layers + 2):
             self.layers[i] = self.activation_func(np.dot(self.layers[i - 1], self.weights[i - 1]))
-
-        return self.layers[self.count_of_layers]
+        if self.classification_mode:
+            self.layers[self.count_of_hidden_layers + 1] = self.layers[self.count_of_hidden_layers + 1].round()
+            predict = list(map(int, self.layers[self.count_of_hidden_layers + 1][0]))
+            return self.recognized_dict.get(''.join(map(str, predict)), "_")
+        else:
+            return self.layers[self.count_of_hidden_layers + 1]
 
     # функции активации
     def _sigmoid(self, x, derivative=False):
